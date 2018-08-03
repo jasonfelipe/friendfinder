8f15a64395c0cd4874e420bb1572a8ef2e130332
Legend <jasonrfelipe@gmail.com>
2018-08-02 18:24:02 -0700

    fixed the route code

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,15 +6,16 @@ module.exports = function (app) {
 
     //Path to Home
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname + "/app/public", 'home.html'));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
-
-    // If no matching route is found default to home
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../app/public", "home.html"));
-    });
+   
     //Path to Survey
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname + "/app/public", "survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
