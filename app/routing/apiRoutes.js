b6e13558af8651e1511964fa465d56f812f9c4b4
Legend <jasonrfelipe@gmail.com>
2018-08-02 21:43:17 -0700

    fixed api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,4 +20,11 @@ module.exports = function (app) {
         return res.json(false);
     });
 
+app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+    console.log(req.body);
+    peopleArray.push(req.body);
+});
 };
