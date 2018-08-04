951d855aa6e607ac210cf932978ce21da17c4e5b
Legend <jasonrfelipe@gmail.com>
2018-08-03 18:10:19 -0700

    finished v1.00, code for match added.

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@
 const peopleArray = [
     {
         name: 'Jason Felipe',
-        photo: 'placeholder.png',
+        photo: 'https://scontent.fphx1-2.fna.fbcdn.net/v/t1.0-9/12278728_10208248400476494_6384796627529133645_n.jpg?_nc_cat=0&oh=157da2e6a67db0e0c9a2d109c56d767e&oe=5C11233E',
         scores: [
             5,
             4,
@@ -15,6 +15,54 @@ const peopleArray = [
             3,
             3,
             4
+        ]
+    },
+    {
+        name: 'Testing1',
+        photo: 'http://pluspng.com/img-png/png-hd-of-stick-figures-stick-figure-png-207.png',
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        name: 'Testing2',
+        photo: 'http://pluspng.com/img-png/png-hd-of-stick-figures-file-dclubley-stick-figure-png-480.png',
+        scores: [
+            1,
+            1,
+            1,
+            2,
+            3,
+            2,
+            3,
+            4,
+            5,
+            4
+        ]
+    },
+    {
+        name: 'Testing3',
+        photo: 'http://pluspng.com/img-png/png-hd-of-stick-figures-file-runner-stickman-png-1167.png',
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
         ]
     }
 ];
